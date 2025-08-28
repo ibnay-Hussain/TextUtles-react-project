0c8ef4608f53f347843f0230f6001a2b3e24ef7f
ibnay <zainishaaah@gmail.com>
2025-08-28 19:36:42 +0500

    fix alert position

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Alert({alert}) {
   return (
     <>
-    {alert && <span className={`alert alert-${alert.type}`} role="alert">
-        {alert.msg}
-    </span>}
+      <div style={{height:'20px'}}>
+        {alert && <div className={`alert alert-${alert.type}`} role="alert">
+            {alert.msg}
+        </div>}
+      </div>
     </>
   )
 }
