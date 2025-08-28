49537273a8993826c868a9d11b584b243487be80
ibnay <zainishaaah@gmail.com>
2025-08-28 19:34:32 +0500

    update aboutus content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -37,11 +37,11 @@ export default function About({ mode, toggleMode }) {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
-                Accordion Item #1
+                Analyze your text
                 <span style={arrowColor} className="ms-auto">▼</span>
                 </button>
                 </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                 <div className="accordion-body">
                     <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                 </div>
@@ -50,7 +50,7 @@ export default function About({ mode, toggleMode }) {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">
-                Accordion Item #1
+                Free to use
                 <span style={arrowColor} className="ms-auto">▼</span>
                 </button>
                 </h2>
@@ -63,7 +63,7 @@ export default function About({ mode, toggleMode }) {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree">
-                Accordion Item #1
+                Browser Compatibility
                 <span style={arrowColor} className="ms-auto">▼</span>
                 </button>
                 </h2>
